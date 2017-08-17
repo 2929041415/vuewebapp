a2183ee957fb95e123cc52d97f46262371546e4b
2929041415 <2929041415@qq.com>
2017-08-17 21:56:58 +0800

    leijie
    inti pro

---
+++ src/components/MainLayOut/Main.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="layout">
+    <Menu mode="horizontal" theme="dark" active-name="1">
+      <div class="layout-logo">
+        <img src="../../assets/logo.png">
+      </div>
+      <div class="layout-nav">
+        <Menu-item name="1">
+          <Icon type="ios-navigate"></Icon>
+          首页
+        </Menu-item>
+      </div>
+    </Menu>
+  </div>
+</template>
+<script>
+  export default {
+
+  }
+</script>
+<style scoped>
+  @import "../../style/layout.css";
+</style>
---
+++ src/modules/index/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <MainLayOut></MainLayOut>
+    <div class="indexstyle">
+      <Carousel v-model="value1" :height="850">
+        <Carousel-item>
+          <div class="demo-carousel">
+            <img src="../../assets/img/66.jpg">
+          </div>
+        </Carousel-item>
+        <Carousel-item>
+          <div class="demo-carousel">
+            <img src="../../assets/img/22.jpg">
+          </div>
+        </Carousel-item>
+        <Carousel-item>
+          <div class="demo-carousel">
+            <img src="../../assets/img/33.jpg">
+          </div>
+        </Carousel-item>
+        <Carousel-item>
+          <div class="demo-carousel">
+            <img src="../../assets/img/44.jpg">
+          </div>
+        </Carousel-item>
+      </Carousel>
+    </div>
+  </div>
+</template>
+<script>
+  import MainLayOut from '../../components/MainLayOut/Main'
+
+  export default {
+    data () {
+      return {
+        value1: 0
+      }
+    },
+    components: { MainLayOut }
+  }
+</script>
+<style scoped>
+  .indexstyle{
+
+  }
+</style>
